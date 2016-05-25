0163212106dc56c28583656802a50a0fc82cc3a0
YesConSan <yesiconsan15@gmail.com>
2016-05-25 09:42:26 -0500

    subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,9 +1,9 @@
 //JavaScript
 $(document).ready(function(e) {
-    $('#pricipal').height($('#page1').height());
+    $('#principal').height($('#page1').height());
 	document.addEventListener("deviceready",function(){
 		
-		audio =window .plugins .LowLatencyAudio;
+		audio=window .plugins .LowLatencyAudio;
 		
 		audio.preloadFX ('do','audio/DO.mp3',function(){},function(e){alert('Error'+e);});
 		audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error'+e);});
@@ -11,16 +11,16 @@ $(document).ready(function(e) {
 		audio.preloadFX('fa','audio/FA.mp3',function(){},function(e){alert('Error'+e);});
 		audio.preloadFX('sol','audio/SOL.mp3',function(){},function(e){alert('Error'+e);});
 		audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error'+e);});
-        audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error'+e);});
+		audio.preloadFX('si','audio/SI.mp3',function(){},function(e){alert('Error'+e);});
 		
 		$('.nota').bind('touchstart',function(){
-			  $(this).addClass('tocada');
-			  audio.play($(this).attr('id'));
-			  
-		}).bind('tochend',function(){
-			$(this).removeClass('tocada');
-            });
+			$(this).addClass('tocada');
+			audio.play($(this).attr('id'));
 			
-	 },false);
+		}).bind('touchend',function(){
+			$(this).removeClass('tocada');
+		});
+		
+	},false);
+	
 });
-
